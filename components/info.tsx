6c984cb4eee14ed73a8d154d13aa2d2cc26b6b30
rdush <rdush824@gmail.com>
2023-10-15 23:41:11 +0300

    add to card

--- components/info.tsx
+++ components/info.tsx
@@ -5,21 +5,21 @@ import { ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { Product } from "@/types";
-// import useCart from "@/hooks/use-cart";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product
 };
 
 const Info: React.FC<InfoProps> = ({ data }) => {
-//   const cart = useCart();
+  const cart = useCart();
 
-//   const onAddToCart = () => {
-//     cart.addItem(data);
-//   }
+  const onAddToCart = () => {
+    cart.addItem(data);
+  }
 
-  return ( 
-      <div>
+  return (
+    <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
       <div className="mt-3 flex items-end justify-between">
         <p className="text-2xl text-gray-900">
@@ -40,9 +40,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-              <Button
-                //   onClick={onAddToCart}
-                  className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart size={20} />
         </Button>
